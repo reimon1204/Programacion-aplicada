8dfc48bbcb93da10f996ead0629e26cdc1b5772b
Ferley Raigoso <73854362+reimon1204@users.noreply.github.com>
2023-08-22 09:10:47 -0500

    Add files via upload

---
+++ nuevos-programas/tercer-programa.py
@@ -0,0 +1,9 @@
+#se define una variable, en la cual se guardara el nombre de la comida del desayuno
+comida="arroz con huevo"
+print("El desayuno es: "+ comida)
+#Actulaize la variable "comida" pero para un almuerzo
+comida="carne molida y frijoles"
+print("El almuerzo es: "+ comida)
+#Actulaize nuevamente la variable "comida" pero para la cena
+comida="pan con chocolate"
+print("La cena es: "+ comida)
