20b12aee6ea1540319a509ddcdec3282b3b9d0ab
Ferley Raigoso <73854362+reimon1204@users.noreply.github.com>
2023-08-22 09:30:41 -0500

    Add files via upload

---
+++ nuevos-programas/septimo-programa.py
@@ -0,0 +1,7 @@
+#Operador para obteener el residuo de una division
+#29 entre 5, da un valor de 5 con un resido de 4, por lo que el valor arrojado es 4(el residuo de dicha division)
+print(29%5)
+# Imprime 2 porque 32 / 3 es 10 con un residuo de 2
+print(32%3)
+#Si la division es exacta y no tiene residuo, el valor arrojado sera 0
+print(44%2)
