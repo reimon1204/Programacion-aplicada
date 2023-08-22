e7a28d4ddef86c13f01fb2d598e7e83ed5a3c82a
Ferley Raigoso <73854362+reimon1204@users.noreply.github.com>
2023-08-22 09:26:25 -0500

    Add files via upload

---
+++ nuevos-programas/sexto-programa.py
@@ -0,0 +1,8 @@
+#Operacion de elevar( 2 elevado a la decima potencia) 2^10=1024
+print(2**10)
+print(8 ** 2)
+#Operacion de 9 elevado al cubo(729)
+print(9**3)
+#Tambien es funcional para exponentes fraccionarios(raices)
+#4 elevado a 0.5(raiz cuadrada)
+print(4**0.5)
