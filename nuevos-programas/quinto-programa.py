7b6ba6162df1199ef0f589bbd33d4f1521e02de1
Ferley Raigoso <73854362+reimon1204@users.noreply.github.com>
2023-08-22 09:21:42 -0500

    Add files via upload

---
+++ nuevos-programas/quinto-programa.py
@@ -0,0 +1,12 @@
+#Operaciones matematicas entre variables
+precio_cafe=3000
+numero_cafes=5
+print(precio_cafe*numero_cafes)
+print(precio_cafe)
+print(numero_cafes)
+#Actulalize el precio del cafe
+precio_cafe=3500
+#Opere nuevamente e imprima los valores
+print(precio_cafe*numero_cafes)
+print(precio_cafe)
+print(numero_cafes)
