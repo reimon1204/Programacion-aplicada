510266e501f9e9685ffc9d21398f3687243a6b9c
Ferley Raigoso <73854362+reimon1204@users.noreply.github.com>
2023-10-03 09:43:30 -0500

    Add files via upload

---
+++ 2_corte/diccionarios_4.py
@@ -0,0 +1,121 @@
+# #### Obtener una clave
+# #puedes acceder a los valores que contiene proporcionando la clave:
+
+building_heights = {"Burj Khalifa": 828, "Shanghai Tower": 632, "Abraj Al Bait": 601, "Ping An": 599, "Lotte World Tower": 554.5, "One World Trade": 541.3}
+print(building_heights["Burj Khalifa"]) # Prints 828
+print(building_heights["Ping An"]) # Prints 599
+
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
+print(zodiac_elements["earth"])
+print(zodiac_elements["fire"])
+
+#Obtener una clave no válida
+
+building_heights = {"Burj Khalifa": 828, "Shanghai Tower": 632, "Abraj Al Bait": 601, "Ping An": 599, "Lotte World Tower": 554.5, "One World Trade": 541.3}
+#print(building_heights["Landmark 81"])
+
+
+#Una forma de evitar este error es verificar primero si la clave existe en el diccionario:
+key_to_check = "Landmark 81"
+
+if key_to_check in building_heights:
+    print(building_heights["Landmark 81"])
+
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
+
+zodiac_elements["energy"] = "Not a Zodiac element"
+
+if "energy" in zodiac_elements:
+   print(zodiac_elements["energy"])
+
+# ##Obtenga una llave de forma segura
+building_heights = {"Burj Khalifa": 828, "Shanghai Tower": 632, "Abraj Al Bait": 601, "Ping An": 599, "Lotte World Tower": 554.5, "One World Trade": 541.3}
+
+# #esta línea devolverá 632:
+building_heights.get("Shanghai Tower")
+
+# #esta línea devolverá Ninguno:
+building_heights.get("My House")
+
+# ###
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+user_ids.get("teraCoder")
+
+if user_ids.get("teraCoder") == None:
+    tc_id = 1000
+else: 
+    tc_id = user_ids.get("teraCoder")
+
+print(tc_id)
+
+if user_ids.get("superStackSmash") == None:
+      stack_id = 100000
+
+print(stack_id)
+
+# ###Eliminar una clave
+#.pop() funciona para eliminar elementos de un diccionario, cuando conoce el valor clave.
+raffle = {223842: "Teddy Bear", 872921: "Concert Tickets", 320291: "Gift Basket", 412123: "Necklace", 298787: "Pasta Maker"}
+print(raffle.pop(320291, "No Prize"))
+## Imprime "Cesta de regalo"
+print(raffle)
+#Prints {223842: "Teddy Bear", 872921: "Concert Tickets", 412123: "Necklace", 298787: "Pasta Maker"}
+print(raffle.pop(100000, "No Prize"))
+# # devolver "No Prize"
+print(raffle)
+#Prints {223842: "Teddy Bear", 872921: "Concert Tickets", 412123: "Necklace", 298787: "Pasta Maker"}
+print(raffle.pop(872921, "No Prize"))
+# # Imprime "Concert Tickets"
+print(raffle)
+# Prints {223842: "Teddy Bear", 412123: "Necklace", 298787: "Pasta Maker"}
+
+available_items = {"health potion": 10, "cake of the cure": 5, "green elixir": 20, "strength sandwich": 25, "stamina grains": 15, "power stew": 30}
+health_points = 20
+
+health_points += available_items.pop("stamina grains", 0)
+health_points += available_items.pop("power stew", 0)
+health_points += available_items.pop("mystic bread", 0)
+
+print(available_items)
+print(health_points)
+
+# ##Obtener todas las claves
+test_scores = {"Grace":[80, 72, 90], "Jeffrey":[88, 68, 81], "Sylvia":[80, 82, 84], "Pedro":[98, 96, 95], "Martin":[78, 80, 78], "Dina":[64, 60, 75]}
+print(list(test_scores))
+# Imprimira ["Grace", "Jeffrey", "Sylvia", "Pedro", "Martin", "Dina"]
+
+for student in test_scores.keys():
+  print(student)
+###
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+users = user_ids.keys()
+lessons = num_exercises.keys()
+
+print(users)
+print(lessons)
+
+# ##Obtener todas las claves, 2 ejemplo
+test_scores = {"Grace":[80, 72, 90], "Jeffrey":[88, 68, 81], "Sylvia":[80, 82, 84], "Pedro":[98, 96, 95], "Martin":[78, 80, 78], "Dina":[64, 60, 75]}
+for score_list in test_scores.values():
+  print(score_list)
+  
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+total_exercises = 0
+
+for exercises in num_exercises.values():
+   total_exercises += exercises
+print(total_exercises)
+
+### ##Obtener todos los objetos
+biggest_brands = {"Apple": 184, "Google": 141.7, "Microsoft": 80, "Coca-Cola": 69.7, "Amazon": 64.8}
+
+for company, value in biggest_brands.items():
+  print(company + " has a value of " + str(value) + " billion dollars. ")
+
+pct_women_in_occupation = {"CEO": 28, "Engineering Manager": 9, "Pharmacist": 58, "Physician": 40, "Lawyer": 37, "Aerospace Engineer": 9}
+
+for occupation, percentage in pct_women_in_occupation.items():
+   print("Women make up " + str(percentage) + " percent of " + occupation + "s.") 
