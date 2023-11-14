b7334d96b214bf6dbccc9b248075734dcd81d8bf
Ferley Raigoso <73854362+reimon1204@users.noreply.github.com>
2023-11-13 22:18:52 -0500

    Create Getting_started.py

---
+++ 2_corte/Codigos_numpy/Types_D/Getting_started.py
@@ -0,0 +1,14 @@
+import numpy
+arr = numpy.array([1, 2, 3, 4, 5])
+print(arr)
+
+#o podemos importar numpy con el alias np
+
+import numpy as np
+arr = np.array([1,2,3,4,5,6,7])
+print(arr)
+
+#Podemos comprobar la versión de NumPy con
+
+import numpy as np
+print(np.__version__)
