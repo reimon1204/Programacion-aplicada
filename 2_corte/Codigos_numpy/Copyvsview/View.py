01b46d8dc8b09b895b6d3a6392694ada42cb6f30
Ferley Raigoso <73854362+reimon1204@users.noreply.github.com>
2023-11-13 22:10:04 -0500

    Create View.py

---
+++ 2_corte/Codigos_numpy/Copyvsview/View.py
@@ -0,0 +1,9 @@
+#Creamos una vista, cambiamos la matriz original y mostramos ambas matrices
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+x = arr.view()
+arr[0] = 42
+print(arr)
+print(x)
