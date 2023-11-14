f22f2307b2c1e5a6d523dc5ebe8fb035908b2b69
Ferley Raigoso <73854362+reimon1204@users.noreply.github.com>
2023-11-13 22:07:55 -0500

    Create Copy.py

---
+++ 2_corte/Codigos_numpy/Copyvsview/Copy.py
@@ -0,0 +1,9 @@
+#Se hace una copia, cambiamos la matriz original y mostramos ambas matrices
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+x = arr.copy()
+arr[0] = 42
+print(arr)
+print(x)
