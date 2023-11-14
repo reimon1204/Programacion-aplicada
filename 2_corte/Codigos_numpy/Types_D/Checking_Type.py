e932d404eddb0d31a97c20aad2026806b3788c2a
Ferley Raigoso <73854362+reimon1204@users.noreply.github.com>
2023-11-13 22:15:59 -0500

    Create Checking_Type.py

---
+++ 2_corte/Codigos_numpy/Types_D/Checking_Type.py
@@ -0,0 +1,11 @@
+#Ejemplo 1, asi obtenemos el tipo de datos
+
+import numpy as np
+arr = np.array([1, 2, 3, 4])
+print(arr.dtype)
+
+#Ejemplo 2, obtenemos el tipo de datos de una matriz con cadenas
+
+import numpy as np
+arr = np.array(['apple', 'banana', 'cherry'])
+print(arr.dtype)
