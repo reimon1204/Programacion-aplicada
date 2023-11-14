136972df77f1f95f010b5d2ed3400e0fe990a445
Ferley Raigoso <73854362+reimon1204@users.noreply.github.com>
2023-11-13 21:46:56 -0500

    Create Access_Array.py

---
+++ 2_corte/Codigos_numpy/Array_indexing/Access_Array.py
@@ -0,0 +1,17 @@
+#Ejemplo 1
+
+import numpy as np
+arr = np.array([1, 2, 3, 4])
+print(arr[0])
+
+#Ejemplo 2
+
+import numpy as np
+arr = np.array([1, 2, 3, 4])
+print(arr[1])
+
+#Ejemplo 3
+
+import numpy as np
+arr = np.array([1, 2, 3, 4])
+print(arr[2] + arr[3])
